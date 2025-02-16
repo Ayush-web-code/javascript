ee9517eda8a91cd2f22a8ac0872987c6cd224e91
Ayush-web-code <ayushsingh993522@gmail.com>
2025-02-16 17:16:33 +0000

    datatypeSummaryshort

--- 01_js/02_convers.js
+++ 01_js/02_convers.js
@@ -10,3 +10,43 @@ console.log(booleanIsLogggedIn)
 //""=> false
 // "hii"=> true
 // 1=>true
+
+//88888888888 **************** operation********************
+let value=3
+let negvalue=-value
+console.log(negvalue)
+
+let str1="hii"
+let str2="name"
+let str3=str1+str2
+console.log(str3)
+console.log(1+"2")
+console.log(1+"2" +"2")
+console.log(1+2+"2")
+console.log("1"+"2")
+
+console.log(+true)
+// console.log(true+)
+let n1,n2,n3;
+n1=n2=n3=3+4
+
+let x=3;
+const y=x++;
+const z=++x;
+// console.log(`x:$(x) ,y:$(y)`)
+console.log(`x:${z} ,y:${y}`)
+
+let a=3;
+// const y=x++;
+const b=++a;
+// console.log(`x:$(x) ,y:$(y)`)
+console.log(`x:${a} ,y:${b}`)
+
+console.log(null>0)// false
+console.log(null==0)//false
+console.log(null>=0) //true
+
+console.log(undefined>0)// false
+console.log(undefined==0)//false
+console.log(undefined>=0) //true
+//avoid this type of conversion i n the code
---
+++ 01_js/datatypeSummary.js
@@ -0,0 +1,5 @@
+const id=symbol('123');
+const anotherId=symbol('123');
+
+console.log(id===anotherId);// not same return vslue not same if input is same
+const bigInt=11111111111111111n//type is undefined
