0e6e87049eac95fc6107aa8372615a074717f1c1
Ayush-web-code <ayushsingh993522@gmail.com>
2025-02-16 16:15:17 +0000

    conversion of datatype

---
+++ 01_js/02_convers.js
@@ -0,0 +1,12 @@
+let age=25
+console.log(typeof(age))
+// nan is also a number but doesnot contain a value
+//"33"=>33
+// "33abc"=>nan type is an Number  
+// true =>1
+let  isLoggedIn="hii"
+let booleanIsLogggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLogggedIn)
+//""=> false
+// "hii"=> true
+// 1=>true
